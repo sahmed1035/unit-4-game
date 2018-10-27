62b2a903d9dfde073e6388f041046229995e9156
sahmed <sahmed@alumni.depaul.edu>
2018-10-26 21:19:46 -0500

    still working

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # unit-4-game
 Crystal Collector Assignment-4 Interactive game for web browsers with the jQuery library. 
+
+The CrystalsCollector game:
+
+fun and interactive game for web browsers that dynamically updates  HTML pages with the jQuery library.
+-Arrays
+-Random Number Generator
+
+
+
+The player will have to guess the random generated number answer. 
+
+-Here's how the app works:
+
+   
+   -The player will be shown a random number at the start of the game.
+   -There are four crystals buttons are displayed.
+
+   -When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+    -The game will hide this amount until the player clicks a crystal.
+    -When they do click one, update the player's score counter.
+
+   -The player wins if their total score matches the random number from the beginning of the game.
+
+   -The player loses if their score goes above the random number.
+
+   -The game restarts whenever the player wins or loses.
+
+    - When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+
+   The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+   -The random number shown at the start of the game should be between 19 - 120.
+   -Each crystal should have a random hidden value between 1 - 12.
---
+++ assets/javascrip/game.js
@@ -0,0 +1,158 @@
+//assigned computerNumPick variables to random number
+var computerNumPick= Math.floor(Math.random()* 101)+19;
+
+//assigned all four crsytal variables to random numbers
+
+var sapphire = Math.floor(Math.random()*12)+1; 
+//random number from 1 to 12
+var emerald = Math.floor(Math.random()*12)+1; 
+var amethyst = Math.floor(Math.random()*12)+1; 
+var ruby = Math.floor(Math.random()*12)+1; 
+       
+//set total user score variable to 0 
+var userTotalScore = 0; 
+
+//set wins and losses variables to 0
+var wins = 0;  
+// going forward it will increment based off of the checkForWinsOrLosses method.
+var losses = 0; 
+
+
+
+$(document).ready(function(){
+
+
+function initial()
+{
+    $("#random-area").html(computerNumPick);
+ 
+}
+function resetGame()
+{
+   //reset computerNumPick to new random picks
+    computerNumPick= Math.floor(Math.random()* 101)+19;
+
+   //reset all crystal variables to new random pick
+    sapphire = Math.floor(Math.random()*12)+1;
+    //random number from 1 to 12
+    emerald = Math.floor(Math.random()*12)+1; 
+    amethyst = Math.floor(Math.random()*12)+1;
+    ruby = Math.floor(Math.random()*12)+1; 
+
+   //Total user  is zero
+   userTotalScore = 0; 
+   playGame();
+}
+
+function checkForWinsOrLosses()   {
+    if(userTotalScore === computerNumPick) {
+        //increment wins by 1.
+        wins++;
+         //win-area div display You're a winner!
+         $("#win-area").html( "You're a winner!"+ "<br> Wins:" + wins); 
+        
+        resetGame();
+    }
+    else if (userTotalScore > computerNumPick) {
+        //increment losses by 1.
+        losses++;
+        //win-area div display You're a winner!
+        $("#win-area").html( "You're a Loser!" + "<br> Losses:" +losses);  
+       
+        resetGame();
+    
+            }
+ 
+        }
+
+
+
+function playGame(){
+
+   
+ /////////////////user's click on sapphire/////////////////
+    $("#sapphire").on("click",function(){
+        //global variable sapphire value added to the userTotalScore
+        userTotalScore += sapphire;
+        //score-title div to display userTotalScore
+        $("#score-area").html(userTotalScore);
+        checkForWinsOrLosses();
+        });
+
+ /////////////////user's click on emerald/////////////////
+ $("#emerald").on("click",function(){
+    //global variable emerald value added to the userTotalScore
+    userTotalScore += emerald;
+    //score-title div to display userTotalScore
+    $("#score-area").html(userTotalScore);
+    checkForWinsOrLosses();
+    });
+
+/////////////////user's click on amethyst/////////////////
+$("#amethyst").on("click",function(){
+    //global variable amethyst value added to the userTotalScore
+    userTotalScore += amethyst;
+    //score-title div to display userTotalScore
+    $("#score-area").html(userTotalScore);  
+    checkForWinsOrLosses();
+    });
+
+
+/////////////////user's click on ruby/////////////////
+  $("#ruby").on("click",function(){
+    //global variable ruby value added to the userTotalScore
+    userTotalScore += ruby;
+    //score-title div to display userTotalScore
+    $("#score-area").html(userTotalScore);  
+    checkForWinsOrLosses() 
+    
+    });
+
+
+
+ }
+
+    
+
+
+
+initial();
+
+playGame();
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
